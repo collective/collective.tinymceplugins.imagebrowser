ea3921cb189312e7f7a8d75aec1597e6e06dd3b5
Joscha Krutzki <joka@jokasis.de>
2011-04-14 19:59:31 +0200

    updated docu

--- collective/tinymceplugins/imagebrowser/interfaces.py
+++ collective/tinymceplugins/imagebrowser/interfaces.py
@@ -10,7 +10,10 @@ class ITinyMCELibrariesExtended(Interface):
     imagebrowser_ressources = schema.Text(
         title=_(u"Image Ressources"),
         description=_(u"""Enter a list of ressources to appear in the link image
-            dialog. The format is id|title|path_to_folder, one per line."""),
+            dialog. The format is id|title|path_to_folder|path_to_icon, one per
+            line. An example path_to_folder is "PloneRoot/pictures", an example
+            path_to_icon is "/logoIcon.gif". The path_to_icon is optional.
+            """),
         required=False)    
                            
 
