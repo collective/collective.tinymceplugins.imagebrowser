00f98f0d3a983f7bab78c6276a56f9085c97a64e
Joscha Krutzki <joka@jokasis.de>
2011-11-24 17:06:13 +0100

    upgrade step to migrate to plone.registry

--- collective/tinymceplugins/imagebrowser/upgrades.py
+++ collective/tinymceplugins/imagebrowser/upgrades.py
@@ -1,5 +1,20 @@
+from zope.component import getUtility
+
 from plone.app.upgrade.utils import loadMigrationProfile
+from Products.TinyMCE.interfaces.utility import ITinyMCE
+
+from collective.tinymceplugins.imagebrowser.browser.interfaces import \
+        ITinyMCELibrariesExtended
 
 
 def to1beta3(context):
     loadMigrationProfile(context, 'profile-collective.tinymceplugins.imagebrowser:to1beta3')
+
+
+def to1beta4(context):
+    tmce = getUtility(ITinyMCE)
+    adapter = ITinyMCELibrariesExtended(tmce)
+    if hasattr(tmce, "_imagebrowser_ressources"):
+        resources_old = tmce._imagebrowser_ressources
+        adapter.imagebrowser_resources = resources_old
+        delattr(tmce, "_imagebrowser_ressources")
